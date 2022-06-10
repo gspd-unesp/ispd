5632b74733d828b7640e37740bc512decc426de9
Math_RPG <maugusilva.santos@gmail.com>
2022-06-10 13:25:13 -0300

    remove inconsequential methods

--- src/ispd/application/GuiApplication.java
+++ src/ispd/application/GuiApplication.java
@@ -7,19 +7,12 @@ import ispd.gui.SplashWindowBuilder;
 
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.PrintStream;
-import java.util.function.Consumer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class GuiApplication implements Application
 {
     private static final String GUI_LOOK_AND_FEEL_CLASS_NAME = "javax.swing.plaf.nimbus.NimbusLookAndFeel";
-    private static final String ERROR_FILE = String.join(File.separator, "Erros", "Erros_Simulador");
-    private static final String OUTPUT_FILE = String.join(File.separator, "Erros", "Saida_Simulador");
 
     private static void openGui ()
     {
@@ -35,7 +28,6 @@ public class GuiApplication implements Application
         final var exceptionLogger = new LogExceptions(null);
         Thread.setDefaultUncaughtExceptionHandler(exceptionLogger);
 
-        GuiApplication.redirectSystemStreams();
         GuiApplication.setGuiLookAndFeel();
 
         final var mainWindow = GuiApplication.buildMainWindow();
@@ -44,38 +36,6 @@ public class GuiApplication implements Application
         exceptionLogger.setParentComponent(mainWindow);
 
         return mainWindow;
-    }
-
-    private static void redirectSystemStreams ()
-    {
-        redirectStreamToFile(System::setErr, ERROR_FILE);
-        redirectStreamToFile(System::setOut, OUTPUT_FILE);
-    }
-
-    private static void redirectStreamToFile (
-            final Consumer<PrintStream> streamRedirect, final String pathToFile)
-    {
-        var fileStream = GuiApplication.getFileStreamOrNull(pathToFile);
-
-        // TODO: Maybe optional?
-        if (fileStream == null)
-            return;
-
-        var printStream = new PrintStream(fileStream);
-        // TODO: Uncomment once behaviour has been validated
-//        streamRedirect.accept(printStream);
-    }
-
-    private static FileOutputStream getFileStreamOrNull (final String pathToFile)
-    {
-        try
-        {
-            return new FileOutputStream(pathToFile);
-        } catch (FileNotFoundException ex)
-        {
-            GuiApplication.logWithMainLogger(ex);
-            return null;
-        }
     }
 
     private static void setGuiLookAndFeel ()
