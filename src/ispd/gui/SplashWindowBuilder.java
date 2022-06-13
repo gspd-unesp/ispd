1d5bf50b31a5187096800d1d32ba1e0e7ea9e1e6
Math_RPG <maugusilva.santos@gmail.com>
2022-06-12 23:37:56 -0300

    code style housekeeping.

--- src/ispd/gui/SplashWindowBuilder.java
+++ src/ispd/gui/SplashWindowBuilder.java
@@ -2,54 +2,31 @@ package ispd.gui;
 
 import ispd.Main;
 
-import javax.swing.*;
+import javax.swing.ImageIcon;
 import java.net.URL;
 import java.util.MissingResourceException;
 
 public class SplashWindowBuilder
 {
     // TODO: Perhaps over-engineering?
-    private static final String defaultImagePath = "gui/imagens/Splash.gif";
-    private static final String defaultText = "Copyright (c) 2010 - 2014 GSPD.  All rights reserved."; // TODO: 2 spaces?
+    private static final String DEFAULT_IMAGE_PATH = "gui/imagens/Splash.gif";
+    private static final String DEFAULT_TEXT = "Copyright (c) 2010 - 2014 GSPD.  All rights reserved."; // TODO: 2 spaces?
     private String imagePath = "";
     private String text = "";
     private boolean shouldBeVisible = false;
 
     public static SplashWindow visibleDefaultSplashWindow ()
     {
-        return aSplashWindow()
-                .withImage(defaultImagePath)
-                .withText(defaultText)
+        return SplashWindowBuilder.aSplashWindow()
+                .withImage(SplashWindowBuilder.DEFAULT_IMAGE_PATH)
+                .withText()
                 .visible()
                 .build();
     }
 
-    public static SplashWindowBuilder aSplashWindow ()
+    SplashWindow build ()
     {
-        return new SplashWindowBuilder();
-    }
-
-    public SplashWindowBuilder withImage (String imagePath)
-    {
-        this.imagePath = imagePath;
-        return this;
-    }
-
-    public SplashWindowBuilder withText (String text)
-    {
-        this.text = text;
-        return this;
-    }
-
-    public SplashWindowBuilder visible ()
-    {
-        this.shouldBeVisible = true;
-        return this;
-    }
-
-    public SplashWindow build ()
-    {
-        final var window = new SplashWindow(getImage());
+        final var window = new SplashWindow(this.getImage());
         window.setText(this.text);
 
         if (this.shouldBeVisible)
@@ -60,20 +37,41 @@ public class SplashWindowBuilder
         return window;
     }
 
+    private SplashWindowBuilder visible ()
+    {
+        this.shouldBeVisible = true;
+        return this;
+    }
+
+    private SplashWindowBuilder withText ()
+    {
+        this.text = SplashWindowBuilder.DEFAULT_TEXT;
+        return this;
+    }
+
+    SplashWindowBuilder withImage (final String imagePath)
+    {
+        this.imagePath = imagePath;
+        return this;
+    }
+
+    static SplashWindowBuilder aSplashWindow ()
+    {
+        return new SplashWindowBuilder();
+    }
+
     private ImageIcon getImage ()
     {
         final var imageUrl = Main.class.getResource(this.imagePath);
 
-        if (imageUrl == null)
-        {
-            // TODO: Study MRE arguments
-            throw new MissingResourceException(
-                    "Missing .gif for splash window",
-                    URL.class.getName(),
-                    this.imagePath
-            );
-        }
+        if (null != imageUrl)
+            return new ImageIcon(imageUrl);
 
-        return new ImageIcon(imageUrl);
+        // TODO: Study MRE arguments
+        throw new MissingResourceException(
+                "Missing .gif for splash window",
+                URL.class.getName(),
+                this.imagePath
+        );
     }
 }
