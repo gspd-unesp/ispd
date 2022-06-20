24b42ed63c9a5671e4a016c7409be593a8bdae64
Math_RPG <maugusilva.santos@gmail.com>
2022-06-20 16:39:25 -0300

    move inner class to outer scope.

---
+++ src/ispd/gui/auxiliar/UserOperationTime.java
@@ -0,0 +1,45 @@
+package ispd.gui.auxiliar;
+
+/**
+ * Note: this class has a natural ordering that is inconsistent with equals.
+ */
+class UserOperationTime implements Comparable<UserOperationTime> {
+    // TODO: make this should be a record
+
+    private final Double time;
+    private final Boolean isStartTime;
+    private final Double nodeUse;
+    private final Integer userId;
+
+    UserOperationTime(
+            final double time,
+            final boolean isStartTime,
+            final Double nodeUse,
+            final Integer userId) {
+        this.time = time;
+        this.isStartTime = isStartTime;
+        this.nodeUse = nodeUse;
+        this.userId = userId;
+    }
+
+    Integer getUserId() {
+        return this.userId;
+    }
+
+    Boolean isStartTime() {
+        return this.isStartTime;
+    }
+
+    Double getNodeUse() {
+        return this.nodeUse;
+    }
+
+    @Override
+    public int compareTo(final UserOperationTime o) {
+        return this.time.compareTo(o.getTime());
+    }
+
+    Double getTime() {
+        return this.time;
+    }
+}
