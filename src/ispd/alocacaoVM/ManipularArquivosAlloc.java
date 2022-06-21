f4fdd20c282ee9d6af80213596bdd9dc9f9ff901
Math_RPG <maugusilva.santos@gmail.com>
2022-06-21 13:25:38 -0300

    Reformat ManipularArquivosAlloc.java.

--- src/ispd/alocacaoVM/ManipularArquivosAlloc.java
+++ src/ispd/alocacaoVM/ManipularArquivosAlloc.java
@@ -1,33 +1,25 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package ispd.alocacaoVM;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * @author denison_usuario
- */
-public interface ManipularArquivosAlloc
-{
-    ArrayList<String> listar ();
+public interface ManipularArquivosAlloc {
+    ArrayList<String> listar();
 
-    File getDiretorio ();
+    File getDiretorio();
 
-    boolean escrever (String nome, String codigo);
+    boolean escrever(String nome, String codigo);
 
-    String compilar (String nome);
+    String compilar(String nome);
 
-    String ler (String alocador);
+    String ler(String alocador);
 
-    boolean remover (String alocador);
+    boolean remover(String alocador);
 
-    boolean importarAlocadoresJava (File arquivo);
+    boolean importarAlocadoresJava(File arquivo);
 
-    List listarAdicionados ();
+    List listarAdicionados();
 
-    List listarRemovidos ();
+    List listarRemovidos();
 }
