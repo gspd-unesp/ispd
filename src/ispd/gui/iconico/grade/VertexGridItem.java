be51f547e02fea9a3af84a82825db33df262e6e8
rricardof <ricardoxdd28@outlook.com>
2022-06-24 16:31:31 -0300

    Added `VertexGridItem` class
     * VertexGridItem class will be used for reduce the need of
       rewriting the same business logic every time a class extends
       `Vertex` and implements `GridItem`.

---
+++ src/ispd/gui/iconico/grade/VertexGridItem.java
@@ -0,0 +1,150 @@
+package ispd.gui.iconico.grade;
+
+import ispd.gui.iconico.Vertex;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/* package-private */ abstract class VertexGridItem
+        extends Vertex implements GridItem {
+
+    /**
+     * It contains the grid item identifier.
+     */
+    protected final GridItemIdentifier id;
+
+    /**
+     * It stores all of inbound connections.
+     */
+    protected final Set<GridItem> inboundConnections;
+
+    /**
+     * It stores all of outbound connections.
+     */
+    protected final Set<GridItem> outboundConnections;
+
+    /**
+     * It stores the state of whether this vertex is
+     * configured or not. Therefore, this stores {@code true}
+     * if vertex is configured; otherwise, it stores
+     * {@code false}.
+     * <p>
+     * The state of this variable is {@code false} by default,
+     * however, it can be changed by attribution or at
+     * construction.
+     */
+    protected boolean configured;
+
+    /**
+     * Constructor of {@link VertexGridItem} which specifies
+     * the local, global and name identifiers, as well as,
+     * the X and Y coordinates and whether is selected.
+     *
+     * @param localId  the local id
+     * @param globalId the global id
+     * @param name     the name
+     * @param x        the vertex grid item x-coordinate
+     *                 in cartesian coordinates
+     * @param y        the vertex grid item y-coordinate
+     *                 in cartesian coordinates
+     * @param selected whether is selected
+     */
+    public VertexGridItem(
+            final int localId,
+            final int globalId,
+            final String name,
+            final Integer x,
+            final Integer y,
+            final boolean selected) {
+        super(x, y, selected);
+        this.id = new GridItemIdentifier(localId, globalId, name + globalId);
+        this.inboundConnections = new HashSet<>();
+        this.outboundConnections = new HashSet<>();
+    }
+
+    /**
+     * Constructor of {@link VertexGridItem} which specifies
+     * the local, global and name identifiers, as well as,
+     * the X and Y coordinates.
+     *
+     * @param localId  the local id
+     * @param globalId the global id
+     * @param name     the name
+     * @param x        the vertex grid item x-coordinate
+     *                 in cartesian coordinates
+     * @param y        the vertex grid item y-coordinate
+     *                 in cartesian coordinates
+     */
+    public VertexGridItem(
+            final int localId,
+            final int globalId,
+            final String name,
+            final Integer x,
+            final Integer y) {
+        this(localId, globalId, name, x, y, false);
+    }
+
+    /**
+     * Returns this grid item offset.
+     *
+     * @return this grid item offset
+     * @apiNote The offset represents a <em>margin of error</em> to
+     *         state whether this grid item is contained at a given
+     *         x-coordinate and y-coordinate in {@link #contains(int, int)}
+     *         method.
+     */
+    protected int getOffset() {
+        return 17;
+    }
+
+    /**
+     * Returns {@code true} if this grid item is contained at
+     * the given x-coordinate and y-coordinate (in cartesian
+     * coordinates) plus a <em>offset</em>. Otherwise, {@code
+     * false} is returned.
+     *
+     * @param x the X-coordinate
+     * @param y the Y-coordinate
+     * @return {@code true} if this grid item is contained at
+     *         the given coordinates; otherwise {@code false}
+     *         is returned.
+     */
+    @Override
+    public boolean contains(final int x, final int y) {
+        final var offset = this.getOffset();
+        return (x > this.getX() - offset && x < this.getX() + offset) &&
+                (y > this.getY() - offset && y < this.getY() + offset);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public GridItemIdentifier getId() {
+        return this.id;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Set<GridItem> getInboundConnections() {
+        return this.inboundConnections;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Set<GridItem> getOutboundConnections() {
+        return this.outboundConnections;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isConfigured() {
+        return this.configured;
+    }
+}
