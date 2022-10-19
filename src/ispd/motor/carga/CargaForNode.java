baf58a5f7f9d9c2ccc7d3a2daf968dbcfb8117b8
Math_RPG <maugusilva.santos@gmail.com>
2022-10-19 11:59:34 -0300

    More cleanup in CargaForNode.

--- src/ispd/motor/carga/CargaForNode.java
+++ src/ispd/motor/carga/CargaForNode.java
@@ -3,11 +3,11 @@ package ispd.motor.carga;
 import ispd.motor.filas.RedeDeFilas;
 import ispd.motor.filas.Tarefa;
 import ispd.motor.filas.servidores.CS_Processamento;
+import ispd.motor.filas.servidores.CentroServico;
 import ispd.motor.random.Distribution;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.Vector;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -18,23 +18,35 @@ import java.util.stream.IntStream;
 public class CargaForNode extends GerarCarga {
     private static final double FILE_RECEIVE_TIME = 0.0009765625;
     private final String application;
-    private final int taskCount;
-    private final Double minComputation;
-    private final Double maxComputation;
-    private final Double minCommunication;
-    private final Double maxCommunication;
     private final String owner;
-    private final String scheduler;
+    private final String schedulerId;
+    private final int taskCount;
+    private final double maxComputation;
+    private final double minComputation;
+    private final double maxCommunication;
+    private final double minCommunication;
     private int nextAvailableTaskId = 0;
 
+    /**
+     * Create a per-node workload with the given parameters
+     *
+     * @param application      application of the workload
+     * @param owner            name of the user responsible for the workload
+     * @param schedulerId      id of the machine responsible for scheduling this workload's tasks
+     * @param taskCount        total number of tasks
+     * @param maxComputation   computation maximum
+     * @param minComputation   computation minimum
+     * @param maxCommunication communication maximum
+     * @param minCommunication communication minimum
+     */
     public CargaForNode(
             final String application, final String owner,
-            final String scheduler, final int taskCount,
+            final String schedulerId, final int taskCount,
             final double maxComputation, final double minComputation,
             final double maxCommunication, final double minCommunication) {
         this.application = application;
         this.owner = owner;
-        this.scheduler = scheduler;
+        this.schedulerId = schedulerId;
         this.taskCount = taskCount;
         this.maxComputation = maxComputation;
         this.minComputation = minComputation;
@@ -75,59 +87,55 @@ public class CargaForNode extends GerarCarga {
 
     /**
      * Build a {@link Vector} with data from this instance:
-     * {@link #application}, {@link #owner}, {@link #scheduler}, {@link #taskCount},
+     * {@link #application}, {@link #owner}, {@link #schedulerId}, {@link #taskCount},
      * and minimums and maximums for computation and communication.
      * The order of the attributes after {@link #owner} is the same as the one used in this class'
      * {@link #toString()} and {@link #newGerarCarga(String)} methods.
      *
      * @return {@link Vector} with this instance's data.
      */
-    public Vector toVector() {
-        final Vector temp = new Vector<Integer>(8);
+    public Vector<String> toVector() {
+        final var temp = new Vector<String>(8);
         temp.add(0, this.application);
         temp.add(1, this.owner);
-        temp.add(2, this.scheduler);
-        temp.add(3, this.taskCount);
-        temp.add(4, this.maxComputation);
-        temp.add(5, this.minComputation);
-        temp.add(6, this.maxCommunication);
-        temp.add(7, this.minCommunication);
+        temp.add(2, this.schedulerId);
+        temp.add(3, String.valueOf(this.taskCount));
+        temp.add(4, String.valueOf(this.maxComputation));
+        temp.add(5, String.valueOf(this.minComputation));
+        temp.add(6, String.valueOf(this.maxCommunication));
+        temp.add(7, String.valueOf(this.minCommunication));
         return temp;
     }
 
     @Override
     public List<Tarefa> toTarefaList(final RedeDeFilas rdf) {
-        return this.findMasterWithCorrectScheduler(rdf)
-                .map(this::makeTaskListFromMaster)
+        return rdf.getMestres().stream()
+                .filter(this::hasCorrectId)
+                .findFirst()
+                .map(this::makeTaskListOriginatingAt)
                 .orElseGet(ArrayList::new);
     }
 
-    private Optional<CS_Processamento> findMasterWithCorrectScheduler(final RedeDeFilas rdf) {
-        return rdf.getMestres().stream()
-                .filter(this::hasCorrectScheduler)
-                .findFirst();
+    private boolean hasCorrectId(final CS_Processamento m) {
+        return m.getId().equals(this.schedulerId);
     }
 
-    private List<Tarefa> makeTaskListFromMaster(final CS_Processamento master) {
+    private List<Tarefa> makeTaskListOriginatingAt(final CentroServico origin) {
         final var random = new Distribution(System.currentTimeMillis());
 
         return IntStream.range(0, this.taskCount)
-                .mapToObj(i -> this.makeTaskWith(master, random, calculateDelay()))
+                .mapToObj(i -> this.makeTaskOriginatingAt(origin, random))
                 .collect(Collectors.toList());
     }
 
-    private boolean hasCorrectScheduler(final CS_Processamento m) {
-        return m.getId().equals(this.scheduler);
-    }
-
-    private Tarefa makeTaskWith(
-            final CS_Processamento master, final Distribution random, final int delay) {
+    private Tarefa makeTaskOriginatingAt(
+            final CentroServico origin, final Distribution random) {
 
         return new Tarefa(
                 this.nextTaskId(),
                 this.owner,
                 this.application,
-                master,
+                origin,
                 CargaForNode.fromTwoStageUniform(
                         random,
                         this.minCommunication,
@@ -139,12 +147,8 @@ public class CargaForNode extends GerarCarga {
                         this.minComputation,
                         this.maxComputation
                 ),
-                random.nextExponential(5) + delay
+                random.nextExponential(5) + this.calculateDelay()
         );
-    }
-
-    private int calculateDelay() {
-        return "NoDelay".equals(this.owner) ? 120 : 0;
     }
 
     private int nextTaskId() {
@@ -158,6 +162,10 @@ public class CargaForNode extends GerarCarga {
         return random.twoStageUniform(min, min + (max - min) / 2, max, 1);
     }
 
+    private int calculateDelay() {
+        return "NoDelay".equals(this.owner) ? 120 : 0;
+    }
+
     public Integer getNumeroTarefas() {
         return this.taskCount;
     }
@@ -165,7 +173,7 @@ public class CargaForNode extends GerarCarga {
     @Override
     public String toString() {
         return String.format("%s %d %f %f %f %f",
-                this.scheduler, this.taskCount,
+                this.schedulerId, this.taskCount,
                 this.maxComputation, this.minComputation,
                 this.maxCommunication, this.minCommunication
         );
@@ -176,12 +184,12 @@ public class CargaForNode extends GerarCarga {
         return GerarCarga.FORNODE;
     }
 
-    void setInicioIdentificadorTarefa(final int taskIdentifierStart) {
-        this.nextAvailableTaskId = taskIdentifierStart;
+    void setInicioIdentificadorTarefa(final int newNextId) {
+        this.nextAvailableTaskId = newNextId;
     }
 
     public String getEscalonador() {
-        return this.scheduler;
+        return this.schedulerId;
     }
 
     public String getAplicacao() {
