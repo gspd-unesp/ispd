c6934ab199ac362dbe6d2b16f7c6081cfef91ea7
Math_RPG <maugusilva.santos@gmail.com>
2022-11-16 10:38:55 -0300

    Move another inner class out.

--- src/ispd/policy/managers/FilePolicyManager.java
+++ src/ispd/policy/managers/FilePolicyManager.java
@@ -15,14 +15,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
-import java.util.jar.JarFile;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
 
-// TODO: Document
 /* package-private */
 abstract class FilePolicyManager implements PolicyManager {
     private static final String JAR_PREFIX = "jar:";
@@ -69,7 +65,8 @@ abstract class FilePolicyManager implements PolicyManager {
 
     protected abstract String className();
 
-    private static void createDirectory(final File dir) throws IOException {
+    /* package-private */
+    static void createDirectory(final File dir) throws IOException {
         if (!dir.mkdirs()) {
             throw new IOException("Failed to create directory " + dir);
         }
@@ -310,17 +307,11 @@ abstract class FilePolicyManager implements PolicyManager {
         }
     }
 
-    /**
-     * @return added policies
-     */
     @Override
     public List listarAdicionados() {
         return this.addedPolicies;
     }
 
-    /**
-     * @return remove policies
-     */
     @Override
     public List listarRemovidos() {
         return this.removedPolicies;
@@ -332,57 +323,5 @@ abstract class FilePolicyManager implements PolicyManager {
 
     private File fileWithExtension(final String policyName, final String ext) {
         return new File(this.directory(), policyName + ext);
-    }
-
-    private static class JarExtractor {
-        private static final String MOTOR_PKG_PATH = "motor";
-        private final ZipFile jar = new JarFile(new File(
-                System.getProperty("java.class.path")
-        ));
-        private final String targetPackage;
-
-        private JarExtractor(final String targetPackage) throws IOException {
-            this.targetPackage = targetPackage;
-        }
-
-        private void extractDirsFromJar() throws IOException {
-            this.extractDirFromJar(this.targetPackage);
-            this.extractDirFromJar(JarExtractor.MOTOR_PKG_PATH);
-        }
-
-        private void extractDirFromJar(final String dir) throws IOException {
-            final var entries = this.jar.stream()
-                    .filter(e -> e.getName().contains(dir))
-                    .toList();
-
-            for (final var entry : entries) {
-                this.processZipEntry(entry);
-            }
-        }
-
-        private void processZipEntry(final ZipEntry entry) throws IOException {
-            final var file = new File(entry.getName());
-
-            if (entry.isDirectory() && !file.exists()) {
-                FilePolicyManager.createDirectory(file);
-                return;
-            }
-
-            // TODO: Idea; process ALL directories FIRST, and only THEN
-            //  non-directory entries.
-            //  In such a way, this logic (entry without parent directory
-            //  created) may not be necessary anymore
-
-            final var parent = file.getParentFile();
-
-            if (!parent.exists()) {
-                FilePolicyManager.createDirectory(parent);
-            }
-
-            try (final var is = this.jar.getInputStream(entry);
-                 final var os = new FileOutputStream(file)) {
-                is.transferTo(os);
-            }
-        }
     }
 }
