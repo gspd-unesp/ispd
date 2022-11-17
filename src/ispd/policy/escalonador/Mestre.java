7934205b661bd577ffb334d92cde46dd751bcd97
Math_RPG <maugusilva.santos@gmail.com>
2022-11-17 19:13:38 -0300

    Make Mestre use new PolicyMaster interface; cleanup classes that were tangentially affected by the change.

--- src/ispd/policy/escalonador/Mestre.java
+++ src/ispd/policy/escalonador/Mestre.java
@@ -1,38 +1,16 @@
 package ispd.policy.escalonador;
 
-import ispd.motor.Simulation;
 import ispd.motor.filas.Tarefa;
-import ispd.motor.filas.servidores.CS_Processamento;
-import ispd.policy.PolicyCondition;
-
-import java.util.EnumSet;
+import ispd.policy.PolicyMaster;
 
 /**
  * Interface that has methods implemented only in a Master node,
  * these methods will be use by the schedulers.
  */
-public interface Mestre {
-    int ENQUANTO_HOUVER_TAREFAS = 1;
-    int QUANDO_RECEBE_RESULTADO = 2;
-    int AMBOS = 3;
-
+public interface Mestre extends PolicyMaster {
     void enviarTarefa(Tarefa tarefa);
 
     void processarTarefa(Tarefa tarefa);
 
-    void executePolicy();
-
-    void sendMessage(Tarefa task, CS_Processamento machine, int messageType);
-
-    void updateSubordinate(CS_Processamento subordinate);
-
-    EnumSet<PolicyCondition> getPolicyCondition();
-
-    void setPolicyCondition(EnumSet<PolicyCondition> newPolicyCondition);
-
     Tarefa criarCopia(Tarefa get);
-
-    Simulation getSimulation();
-
-    void setSimulation(Simulation newSimulation);
 }
