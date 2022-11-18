cfe05e3b8831a608c956fdaeea599e0ad3fb01b5
Math_RPG <maugusilva.santos@gmail.com>
2022-11-18 13:26:22 -0300

    Revert "Extract generalized interface."
    This reverts commit ebed9388f40f3edf6964e81726057436ebab374d.

--- src/ispd/policy/alocacaoVM/VMM.java
+++ src/ispd/policy/alocacaoVM/VMM.java
@@ -1,12 +1,32 @@
 package ispd.policy.alocacaoVM;
 
+import ispd.motor.Simulation;
+import ispd.motor.filas.Tarefa;
+import ispd.motor.filas.servidores.CS_Processamento;
 import ispd.motor.filas.servidores.implementacao.CS_VirtualMac;
-import ispd.policy.PolicyMaster;
 
 /**
  * Interface that has methods implemented only in a Master node,
  * these methods will be used by the schedulers.
  */
-public interface VMM extends PolicyMaster {
+public interface VMM {
+    int ENQUANTO_HOUVER_VMS = 1;
+    int QUANDO_RECEBE_RETORNO = 2;
+    int DOISCASOS = 3;
+
     void enviarVM(CS_VirtualMac vm);
+
+    void executePolicy();
+
+    void sendMessage(Tarefa task, CS_Processamento machine, int messageType);
+
+    void updateSubordinate(CS_Processamento subordinate);
+
+    int getPolicyCondition();
+
+    void setPolicyCondition(int newPolicyCondition);
+
+    Simulation getSimulation();
+
+    void setSimulation(Simulation newSimulation);
 }
