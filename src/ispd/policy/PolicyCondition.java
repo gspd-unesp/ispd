38671e2713286b87d738b4acdbe7286f5ee25aab
Math_RPG <maugusilva.santos@gmail.com>
2022-11-18 13:26:22 -0300

    Revert "Make change of pseudo-flags to EnumSet"
    This reverts commit 2797d1b4ac6373dd2644a0ead1c359b957630184.

--- src/ispd/policy/PolicyCondition.java
+++
@@ -1,6 +0,0 @@
-package ispd.policy;
-
-public enum PolicyCondition {
-    WHILE_THERE_ARE_RESOURCES,
-    WHEN_RECEIVES_RETURN
-}
--- src/ispd/policy/PolicyMaster.java
+++ src/ispd/policy/PolicyMaster.java
@@ -4,18 +4,20 @@ import ispd.motor.Simulation;
 import ispd.motor.filas.Tarefa;
 import ispd.motor.filas.servidores.CS_Processamento;
 
-import java.util.EnumSet;
-
 public interface PolicyMaster {
+    int ENQUANTO_HOUVER_VMS = 1;
+    int QUANDO_RECEBE_RETORNO = 2;
+    int DOISCASOS = 3;
+
     void executePolicy();
 
     void sendMessage(Tarefa task, CS_Processamento machine, int messageType);
 
     void updateSubordinate(CS_Processamento subordinate);
 
-    EnumSet<PolicyCondition> getPolicyCondition();
+    int getPolicyCondition();
 
-    void setPolicyCondition(EnumSet<PolicyCondition> newPolicyCondition);
+    void setPolicyCondition(int newPolicyCondition);
 
     Simulation getSimulation();
 
