8c08cfc0a0e38f8b3abbd087fb5ce75b4eba56b0
Math_RPG <maugusilva.santos@gmail.com>
2022-11-19 09:16:52 -0300

    Cleanup of Workqueue.

--- src/ispd/policy/externo/Workqueue.java
+++ src/ispd/policy/externo/Workqueue.java
@@ -1,42 +1,3 @@
-/* ==========================================================
- * iSPD : iconic Simulator of Parallel and Distributed System
- * ==========================================================
- *
- * (C) Copyright 2010-2014, by Grupo de pesquisas em Sistemas Paralelos e Distribuídos da Unesp (GSPD).
- *
- * Project Info:  http://gspd.dcce.ibilce.unesp.br/
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- *
- * [Oracle and Java are registered trademarks of Oracle and/or its affiliates. 
- * Other names may be trademarks of their respective owners.]
- *
- * ---------------
- * Workqueue.java
- * ---------------
- * (C) Copyright 2014, by Grupo de pesquisas em Sistemas Paralelos e Distribuídos da Unesp (GSPD).
- *
- * Original Author:  Denison Menezes (for GSPD);
- * Contributor(s):   -;
- *
- * Changes
- * -------
- * 
- * 09-Set-2014 : Version 2.0;
- *
- */
 package ispd.policy.externo;
 
 import ispd.motor.filas.Tarefa;
@@ -48,47 +9,41 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-/**
- * Implementação do algoritmo de escalonamento Workqueue
- * Atribui a proxima tarefa da fila (FIFO)
- * para um recurso que está livre
- * @author denison_usuario
- */
+@SuppressWarnings("unused")
 public class Workqueue extends Escalonador {
-
-    private LinkedList<Tarefa> ultimaTarefaConcluida;
-    private List<Tarefa> tarefaEnviada;
+    private final LinkedList<Tarefa> ultimaTarefaConcluida = new LinkedList<>();
+    private List<Tarefa> tarefaEnviada = null;
 
     public Workqueue() {
-        this.tarefas = new ArrayList<Tarefa>();
-        this.escravos = new ArrayList<CS_Processamento>();
-        this.ultimaTarefaConcluida = new LinkedList<Tarefa>();
+        this.tarefas = new ArrayList<>();
+        this.escravos = new ArrayList<>();
     }
 
     @Override
     public void iniciar() {
-        tarefaEnviada = new ArrayList<Tarefa>(escravos.size());
-        for (int i = 0; i < escravos.size(); i++) {
-            tarefaEnviada.add(null);
+        this.tarefaEnviada = new ArrayList<>(this.escravos.size());
+        for (int i = 0; i < this.escravos.size(); i++) {
+            this.tarefaEnviada.add(null);
         }
     }
 
     @Override
     public Tarefa escalonarTarefa() {
-        if (tarefas.size() > 0) {
-            return tarefas.remove(0);
+        if (!this.tarefas.isEmpty()) {
+            return this.tarefas.remove(0);
         }
         return null;
     }
 
     @Override
     public CS_Processamento escalonarRecurso() {
-        if (!ultimaTarefaConcluida.isEmpty() && !ultimaTarefaConcluida.getLast().isCopy()) {
-            int index = tarefaEnviada.indexOf(ultimaTarefaConcluida.getLast());
+        if (!this.ultimaTarefaConcluida.isEmpty() && !this.ultimaTarefaConcluida.getLast().isCopy()) {
+            final int index =
+                    this.tarefaEnviada.indexOf(this.ultimaTarefaConcluida.getLast());
             return this.escravos.get(index);
         } else {
-            for (int i = 0; i < tarefaEnviada.size(); i++) {
-                if (tarefaEnviada.get(i) == null) {
+            for (int i = 0; i < this.tarefaEnviada.size(); i++) {
+                if (this.tarefaEnviada.get(i) == null) {
                     return this.escravos.get(i);
                 }
             }
@@ -97,44 +52,50 @@ public class Workqueue extends Escalonador {
     }
 
     @Override
-    public List<CentroServico> escalonarRota(CentroServico destino) {
-        int index = escravos.indexOf(destino);
-        return new ArrayList<CentroServico>((List<CentroServico>) caminhoEscravo.get(index));
+    public List<CentroServico> escalonarRota(final CentroServico destino) {
+        final int index = this.escravos.indexOf(destino);
+        return new ArrayList<>((List<CentroServico>) this.caminhoEscravo.get(index));
     }
 
     @Override
     public void escalonar() {
-        CS_Processamento rec = escalonarRecurso();
-        if (rec != null) {
-            Tarefa trf = escalonarTarefa();
-            if (trf != null) {
-                tarefaEnviada.set(escravos.indexOf(rec), trf);
-                if(!ultimaTarefaConcluida.isEmpty()){
-                    ultimaTarefaConcluida.removeLast();
-                }
-                trf.setLocalProcessamento(rec);
-                trf.setCaminho(escalonarRota(rec));
-                mestre.enviarTarefa(trf);
-            }
+        final CS_Processamento rec = this.escalonarRecurso();
+
+        if (rec == null) {
+            return;
         }
+
+        final Tarefa trf = this.escalonarTarefa();
+
+        if (trf == null) {
+            return;
+        }
+
+        this.tarefaEnviada.set(this.escravos.indexOf(rec), trf);
+        if (!this.ultimaTarefaConcluida.isEmpty()) {
+            this.ultimaTarefaConcluida.removeLast();
+        }
+        trf.setLocalProcessamento(rec);
+        trf.setCaminho(this.escalonarRota(rec));
+        this.mestre.enviarTarefa(trf);
     }
-    
+
     @Override
-    public void adicionarTarefa(Tarefa tarefa){
+    public void adicionarTarefa(final Tarefa tarefa) {
         super.adicionarTarefa(tarefa);
-        if(tarefaEnviada.contains(tarefa)){
-            int index = tarefaEnviada.indexOf(tarefa);
-            tarefaEnviada.set(index, null);
-            mestre.executarEscalonamento();
+        if (this.tarefaEnviada.contains(tarefa)) {
+            final int index = this.tarefaEnviada.indexOf(tarefa);
+            this.tarefaEnviada.set(index, null);
+            this.mestre.executarEscalonamento();
         }
     }
-    
+
     @Override
-    public void addTarefaConcluida(Tarefa tarefa) {
+    public void addTarefaConcluida(final Tarefa tarefa) {
         super.addTarefaConcluida(tarefa);
-        ultimaTarefaConcluida.add(tarefa);
-        if (!tarefas.isEmpty()) {
-            mestre.executarEscalonamento();
+        this.ultimaTarefaConcluida.add(tarefa);
+        if (!this.tarefas.isEmpty()) {
+            this.mestre.executarEscalonamento();
         }
     }
 }
