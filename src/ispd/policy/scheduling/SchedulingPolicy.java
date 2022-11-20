a921dae0c7a2e0e8f35ea0485e663c3eaccb4469
Math_RPG <maugusilva.santos@gmail.com>
2022-11-20 11:13:30 -0300

    Pull up methods.

--- src/ispd/policy/scheduling/SchedulingPolicy.java
+++ src/ispd/policy/scheduling/SchedulingPolicy.java
@@ -1,5 +1,6 @@
 package ispd.policy.scheduling;
 
+import ispd.motor.filas.Mensagem;
 import ispd.motor.filas.Tarefa;
 import ispd.motor.filas.servidores.CS_Processamento;
 import ispd.motor.metricas.MetricasUsuarios;
@@ -15,5 +16,55 @@ public abstract class SchedulingPolicy <T extends SchedulingMaster> implements P
     protected List<List> caminhoEscravo = null;
     protected T mestre = null;
 
+    public void setMestre(final T mestre) {
+        this.mestre = mestre;
+    }
+
     public abstract Tarefa escalonarTarefa();
+
+    public List<CS_Processamento> getEscravos() {
+        return this.escravos;
+    }
+
+    public void addEscravo(final CS_Processamento newSlave) {
+        this.escravos.add(newSlave);
+    }
+
+    public void addTarefaConcluida(final Tarefa tarefa) {
+        if (tarefa.getOrigem().equals(this.mestre)) {
+            this.metricaUsuarios.incTarefasConcluidas(tarefa);
+        }
+    }
+
+    public MetricasUsuarios getMetricaUsuarios() {
+        return this.metricaUsuarios;
+    }
+
+    public void setMetricaUsuarios(final MetricasUsuarios metricaUsuarios) {
+        this.metricaUsuarios = metricaUsuarios;
+    }
+
+    public List<List> getCaminhoEscravo() {
+        return this.caminhoEscravo;
+    }
+
+    public void setCaminhoEscravo(final List<List> caminhoEscravo) {
+        this.caminhoEscravo = caminhoEscravo;
+    }
+
+    public List<Tarefa> getFilaTarefas() {
+        return this.tarefas;
+    }
+
+    public void resultadoAtualizar(final Mensagem mensagem) {
+        final int index = this.escravos.indexOf(mensagem.getOrigem());
+        this.filaEscravo.set(index, mensagem.getFilaEscravo());
+    }
+
+    public void adicionarTarefa(final Tarefa tarefa) {
+        if (tarefa.getOrigem().equals(this.mestre)) {
+            this.metricaUsuarios.incTarefasSubmetidas(tarefa);
+        }
+        this.tarefas.add(tarefa);
+    }
 }
--- src/ispd/policy/scheduling/cloud/CloudSchedulingPolicy.java
+++ src/ispd/policy/scheduling/cloud/CloudSchedulingPolicy.java
@@ -1,6 +1,24 @@
 package ispd.policy.scheduling.cloud;
 
+import ispd.motor.filas.servidores.CS_Processamento;
+import ispd.motor.filas.servidores.implementacao.CS_VirtualMac;
 import ispd.policy.scheduling.SchedulingPolicy;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public abstract class CloudSchedulingPolicy extends SchedulingPolicy<CloudMaster> {
+    protected List<CS_Processamento> getVMsAdequadas(
+            final String usuario,
+            final List<? extends CS_Processamento> slaves) {
+        final var escravosUsuario = new LinkedList<CS_Processamento>();
+        for (final var slave : slaves) {
+            final var slaveVM = (CS_VirtualMac) slave;
+
+            if (slave.getProprietario().equals(usuario) && slaveVM.getStatus() == CS_VirtualMac.ALOCADA) {
+                escravosUsuario.add(slave);
+            }
+        }
+        return escravosUsuario;
+    }
 }
