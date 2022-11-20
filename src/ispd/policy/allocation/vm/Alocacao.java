cac893320e7265901ace2e18c58eb34cbb31c955
Math_RPG <maugusilva.santos@gmail.com>
2022-11-20 11:58:38 -0300

    Pull up fields and methods.

--- src/ispd/policy/allocation/vm/Alocacao.java
+++ src/ispd/policy/allocation/vm/Alocacao.java
@@ -1,25 +1,4 @@
 package ispd.policy.allocation.vm;
 
-import ispd.motor.filas.servidores.implementacao.CS_VirtualMac;
-
-import java.util.List;
-
 public abstract class Alocacao extends VmAllocationPolicy {
-    protected List<CS_VirtualMac> maquinasVirtuais = null;
-    protected List<CS_VirtualMac> VMsRejeitadas = null;
-    protected List<List> infoMaquinas = null;
-
-    public abstract CS_VirtualMac escalonarVM();
-
-    public void addVM(final CS_VirtualMac vm) {
-        this.maquinasVirtuais.add(vm);
-    }
-
-    public List<CS_VirtualMac> getMaquinasVirtuais() {
-        return this.maquinasVirtuais;
-    }
-
-    public List<CS_VirtualMac> getVMsRejeitadas() {
-        return this.VMsRejeitadas;
-    }
 }
