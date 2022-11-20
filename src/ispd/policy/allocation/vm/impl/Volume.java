290b12647d196e9ef7eb348e194c37366b7021ed
Math_RPG <maugusilva.santos@gmail.com>
2022-11-20 09:40:10 -0300

    Some cleanup in Volume.

--- src/ispd/policy/allocation/vm/impl/Volume.java
+++ src/ispd/policy/allocation/vm/impl/Volume.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ispd.policy.allocation.vm.impl;
 
 import ispd.motor.filas.servidores.CS_Processamento;
@@ -19,129 +14,119 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-/**
- *
- * @author Diogo Tavares
- */
+@SuppressWarnings("unused")
 public class Volume extends Alocacao {
+    private final Comparator<CS_VirtualMac> comparaReq =
+            new ComparaRequisitos();
+    private final ComparaVolume comparaRec = new ComparaVolume();
+    private boolean fit = false;
+    private int maqIndex = 0;
+    private List<CS_VirtualMac> VMsOrdenadas = null;
+    private List<CS_Processamento> MaqsOrdenadas = null;
 
-    private boolean fit;
-    private int maqIndex;
-    private List<CS_VirtualMac>VMsOrdenadas;
-    private List<CS_Processamento>MaqsOrdenadas;
-    private ComparaRequisitos comparaReq;
-    private ComparaVolume comparaRec;
-    
 
     public Volume() {
-        this.maquinasVirtuais = new ArrayList<CS_VirtualMac>();
-        this.maquinasFisicas = new ArrayList<CS_Processamento>();
-        this.VMsRejeitadas = new ArrayList<CS_VirtualMac>();
-        this.comparaReq = new ComparaRequisitos();
-        this.comparaRec = new ComparaVolume();
-        //this.VMsOrdenadas = new ArrayList<CS_VirtualMac>();
-        //this.MaqsOrdenadas = new ArrayList<CS_Processamento>();
-        
+        this.maquinasVirtuais = new ArrayList<>();
+        this.maquinasFisicas = new ArrayList<>();
+        this.VMsRejeitadas = new ArrayList<>();
     }
 
     @Override
     public void iniciar() {
-        fit = true;
-        maqIndex = 0;
-        VMsOrdenadas = new ArrayList<CS_VirtualMac>(maquinasVirtuais);
-        Collections.sort(VMsOrdenadas, comparaReq); //ordena vms
-        Collections.reverse(VMsOrdenadas);//deixa a ordenação decrescente
-        MaqsOrdenadas = new ArrayList<CS_Processamento>(maquinasFisicas);
-        Collections.sort(MaqsOrdenadas,(Comparator) comparaRec);//ordena recursos por volume
-        Collections.reverse(MaqsOrdenadas); //deixa a ordenação decrescente
-        
-        if(!maquinasFisicas.isEmpty() && !maquinasVirtuais.isEmpty()){
-            escalonar();
+        this.fit = true;
+        this.maqIndex = 0;
+        this.VMsOrdenadas = new ArrayList<>(this.maquinasVirtuais);
+        this.VMsOrdenadas.sort(this.comparaReq); //ordena vms
+        Collections.reverse(this.VMsOrdenadas);//deixa a ordenação decrescente
+        this.MaqsOrdenadas = new ArrayList<>(this.maquinasFisicas);
+        this.MaqsOrdenadas.sort((Comparator) this.comparaRec);
+        Collections.reverse(this.MaqsOrdenadas);
+
+        if (!this.maquinasFisicas.isEmpty() && !this.maquinasVirtuais.isEmpty()) {
+            this.escalonar();
         }
     }
 
     @Override
     public CS_VirtualMac escalonarVM() {
-        return VMsOrdenadas.remove(0);
+        return this.VMsOrdenadas.remove(0);
     }
 
     @Override
     public CS_Processamento escalonarRecurso() {
-        if (fit) {
-            return maquinasFisicas.get(0);
+        if (this.fit) {
+            return this.maquinasFisicas.get(0);
         } else {
-            return maquinasFisicas.get(maqIndex);
+            return this.maquinasFisicas.get(this.maqIndex);
         }
     }
 
     @Override
-    public List<CentroServico> escalonarRota(CentroServico destino) {
-        int index = maquinasFisicas.indexOf(destino);
-        return new ArrayList<CentroServico>((List<CentroServico>) caminhoMaquina.get(index));
+    public List<CentroServico> escalonarRota(final CentroServico destino) {
+        final int index = this.maquinasFisicas.indexOf(destino);
+        return new ArrayList<>((List<CentroServico>) this.caminhoMaquina.get(index));
     }
 
     @Override
     public void escalonar() {
-
-         
-
-        while (!(maquinasVirtuais.isEmpty())) {
+        while (!(this.maquinasVirtuais.isEmpty())) {
             System.out.println("------------------------------------------");
-            int num_escravos;
-            num_escravos = maquinasFisicas.size();
-            
+            int num_escravos = this.maquinasFisicas.size();
 
-            CS_VirtualMac auxVM = escalonarVM();
+            final CS_VirtualMac auxVM = this.escalonarVM();
 
             while (num_escravos >= 0) {
                 if (num_escravos > 0) {//caso existam máquinas livres
-                    CS_Processamento auxMaq = escalonarRecurso(); //escalona o recurso
+                    final CS_Processamento auxMaq = this.escalonarRecurso();
+                    //escalona
+                    // o recurso
                     if (auxMaq instanceof CS_VMM) {
-                        
-                        System.out.println(auxMaq.getId() + " é um VMM, a VM será redirecionada");
-                        auxVM.setCaminho(escalonarRota(auxMaq));
-                        //salvando uma lista de VMMs intermediarios no caminho da vm e seus respectivos caminhos
-                        //CS_VMM maq = (CS_VMM) auxMaq;
-                        //auxVM.addIntermediario(maq);
-                        //List<CS_VMM> inter = auxVM.getVMMsIntermediarios();
-                        //int index = inter.indexOf((CS_VMM) auxMaq);
-                        //ArrayList<CentroServico> caminhoInter = new ArrayList<CentroServico>(escalonarRota(auxMaq));
-                        //auxVM.addCaminhoIntermediario(index, caminhoInter);
-                        System.out.println( auxVM.getId() + " enviada para " + auxMaq.getId());
-                        vmMaster.sendVm(auxVM);
-                        System.out.println("---------------------------------------");
+
+                        System.out.println(auxMaq.getId() + " é um VMM, a VM " +
+                                           "será redirecionada");
+                        auxVM.setCaminho(this.escalonarRota(auxMaq));
+                        //salvando uma lista de VMMs intermediarios no 
+                        // caminho da vm e seus respectivos caminhos
+                        System.out.println(auxVM.getId() + " enviada para " + auxMaq.getId());
+                        this.vmMaster.sendVm(auxVM);
+                        System.out.println(
+                                "---------------------------------------");
                         break;
                     } else {
                         System.out.println("Checagem de recursos:");
-                        CS_MaquinaCloud maq = (CS_MaquinaCloud) auxMaq;
-                        double memoriaMaq = maq.getMemoriaDisponivel();
+                        final CS_MaquinaCloud maq = (CS_MaquinaCloud) auxMaq;
+                        final double memoriaMaq = maq.getMemoriaDisponivel();
                         System.out.println("memoriaMaq: " + memoriaMaq);
-                        double memoriaNecessaria = auxVM.getMemoriaDisponivel();
+                        final double memoriaNecessaria =
+                                auxVM.getMemoriaDisponivel();
                         System.out.println("memorianecessaria: " + memoriaNecessaria);
-                        double discoMaq = maq.getDiscoDisponivel();
+                        final double discoMaq = maq.getDiscoDisponivel();
                         System.out.println("discoMaq: " + discoMaq);
-                        double discoNecessario = auxVM.getDiscoDisponivel();
+                        final double discoNecessario =
+                                auxVM.getDiscoDisponivel();
                         System.out.println("disconecessario: " + discoNecessario);
-                        int maqProc = maq.getProcessadoresDisponiveis();
+                        final int maqProc = maq.getProcessadoresDisponiveis();
                         System.out.println("ProcMaq: " + maqProc);
-                        int procVM = auxVM.getProcessadoresDisponiveis();
+                        final int procVM = auxVM.getProcessadoresDisponiveis();
                         System.out.println("ProcVM: " + procVM);
-                        //System.out.println("---------------------------------------");
 
                         if ((memoriaNecessaria <= memoriaMaq && discoNecessario <= discoMaq && procVM <= maqProc)) {
                             maq.setMemoriaDisponivel(memoriaMaq - memoriaNecessaria);
-                             System.out.println("Realizando o controle de recurso:");
+                            System.out.println("Realizando o controle de " +
+                                               "recurso:");
                             System.out.println("memoria atual da maq: " + (memoriaMaq - memoriaNecessaria));
                             maq.setDiscoDisponivel(discoMaq - discoNecessario);
                             System.out.println("disco atual maq: " + (discoMaq - discoNecessario));
                             maq.setProcessadoresDisponiveis(maqProc - procVM);
                             System.out.println("proc atual: " + (maqProc - procVM));
                             auxVM.setMaquinaHospedeira((CS_MaquinaCloud) auxMaq);
-                            auxVM.setCaminho(escalonarRota(auxMaq));
-                            System.out.println( auxVM.getId() + " enviada para " + auxMaq.getId());
-                            vmMaster.sendVm(auxVM);
-                            System.out.println("---------------------------------------");
-                            atualizarVolume();
+                            auxVM.setCaminho(this.escalonarRota(auxMaq));
+                            System.out.println(auxVM.getId() + " enviada para" +
+                                               " " + auxMaq.getId());
+                            this.vmMaster.sendVm(auxVM);
+                            System.out.println(
+                                    "---------------------------------------");
+                            this.atualizarVolume();
                             break;
 
                         } else {
@@ -151,20 +136,20 @@ public class Volume extends Alocacao {
                 } else {
                     System.out.println(auxVM.getId() + " foi rejeitada");
                     auxVM.setStatus(CS_VirtualMac.REJEITADA);
-                    VMsRejeitadas.add(auxVM);
+                    this.VMsRejeitadas.add(auxVM);
                     System.out.println("Adicionada na lista de rejeitadas");
                     num_escravos--;
-                     System.out.println("---------------------------------------");
+                    System.out.println(
+                            "---------------------------------------");
                 }
             }
         }
 
     }
 
-    public void atualizarVolume(){
-        Collections.sort(MaqsOrdenadas,(Comparator)comparaRec);
-        Collections.reverse(infoMaquinas);
+    private void atualizarVolume() {
+        this.MaqsOrdenadas.sort((Comparator) this.comparaRec);
+        Collections.reverse(this.infoMaquinas);
     }
-    
 }
 
