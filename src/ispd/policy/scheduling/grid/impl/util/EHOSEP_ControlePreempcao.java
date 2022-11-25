b41597aefaa55929a01b8d66c3c28bec8761703a
Math_RPG <maugusilva.santos@gmail.com>
2022-11-25 18:04:39 -0300

    Make duplicate inherit from super.

--- src/ispd/policy/scheduling/grid/impl/util/EHOSEP_ControlePreempcao.java
+++ src/ispd/policy/scheduling/grid/impl/util/EHOSEP_ControlePreempcao.java
@@ -1,19 +1,11 @@
 package ispd.policy.scheduling.grid.impl.util;
 
 //Classe para armazenar dados sobre as preempções que ainda não terminaram
-public class EHOSEP_ControlePreempcao {
-
-    private final String usuarioPreemp;
-    private final String usuarioAlloc;
-    private final int preempID;//ID da tarefa que sofreu preempção
-    private final int allocID;//ID da tarefa alocada
+public class EHOSEP_ControlePreempcao extends PreemptionControl {
 
     public EHOSEP_ControlePreempcao(final String userP, final int pID,
                                     final String userA, final int aID) {
-        this.usuarioPreemp = userP;
-        this.preempID = pID;
-        this.usuarioAlloc = userA;
-        this.allocID = aID;
+        super(userP, pID, userA, aID);
     }
 
     public String getUsuarioPreemp() {
