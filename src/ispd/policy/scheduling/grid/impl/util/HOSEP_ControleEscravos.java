983f285e4b3af371a3e2e1af6894ae22a06eaf98
Math_RPG <maugusilva.santos@gmail.com>
2022-11-25 19:04:04 -0300

    Make class inherit from other.

--- src/ispd/policy/scheduling/grid/impl/util/HOSEP_ControleEscravos.java
+++ src/ispd/policy/scheduling/grid/impl/util/HOSEP_ControleEscravos.java
@@ -6,22 +6,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 //Classe para arnazenar o estado das máquinas no sistema
-public class HOSEP_ControleEscravos {
-
-    private final String ID;//Id da máquina escravo
-    private final int index;//Índice na lista de escravos
-    private String status;//Estado da máquina
-    private ArrayList<Tarefa> fila;
-    private ArrayList<Tarefa> processador;
+public class HOSEP_ControleEscravos extends SlaveControl {
 
     public HOSEP_ControleEscravos(final String Ident, final int ind,
                                   final ArrayList<Tarefa> F,
                                   final ArrayList<Tarefa> P) {
-        this.status = "Livre";
-        this.ID = Ident;
-        this.index = ind;
-        this.fila = F;
-        this.processador = P;
+        super(Ident, ind, F, P);
     }
 
     public String getID() {
