d5ca3ec2ea72de504c22c44a8fd5e625c58d0902
Math_RPG <maugusilva.santos@gmail.com>
2022-11-25 19:11:31 -0300

    Remove redundant methods.

--- src/ispd/policy/scheduling/grid/impl/util/OSEP_ControleEscravos.java
+++ src/ispd/policy/scheduling/grid/impl/util/OSEP_ControleEscravos.java
@@ -6,27 +6,4 @@ public class OSEP_ControleEscravos extends SlaveStatusControl {
         super(ID);
     }
 
-    public String getID() {
-        return this.ID;
-    }
-
-    public String getStatus() {
-        return this.status;
-    }
-
-    public void setOcupado() {
-        this.status = "Ocupado";
-    }
-
-    public void setLivre() {
-        this.status = "Livre";
-    }
-
-    public void setBloqueado() {
-        this.status = "Bloqueado";
-    }
-
-    public void setIncerto() {
-        this.status = "Incerto";
-    }
 }
