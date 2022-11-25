1ba120b1605f316ede03065ca77d2b71b91a0db0
Math_RPG <maugusilva.santos@gmail.com>
2022-11-25 19:00:13 -0300

    Make class inherit other.

--- src/ispd/policy/scheduling/grid/impl/util/M_OSEP_ControlePreempcao.java
+++ src/ispd/policy/scheduling/grid/impl/util/M_OSEP_ControlePreempcao.java
@@ -1,18 +1,9 @@
 package ispd.policy.scheduling.grid.impl.util;
 
-public class M_OSEP_ControlePreempcao {
-
-    private final String usuarioPreemp;
-    private final String usuarioAlloc;
-    private final int preempID;
-    private final int allocID;
-
+public class M_OSEP_ControlePreempcao extends PreemptionControl {
     public M_OSEP_ControlePreempcao(final String user1, final int pID,
                                     final String user2, final int aID) {
-        this.usuarioPreemp = user1;
-        this.preempID = pID;
-        this.usuarioAlloc = user2;
-        this.allocID = aID;
+        super(user1, pID, user2, aID);
     }
 
     public String getUsuarioPreemp() {
