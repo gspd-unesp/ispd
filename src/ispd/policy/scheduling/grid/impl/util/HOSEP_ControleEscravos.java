92decb67322c9bc9144b9f57339a9c567aab6e58
Math_RPG <maugusilva.santos@gmail.com>
2022-11-25 19:04:45 -0300

    Remove redundant methods.

--- src/ispd/policy/scheduling/grid/impl/util/HOSEP_ControleEscravos.java
+++ src/ispd/policy/scheduling/grid/impl/util/HOSEP_ControleEscravos.java
@@ -3,7 +3,6 @@ package ispd.policy.scheduling.grid.impl.util;
 import ispd.motor.filas.Tarefa;
 
 import java.util.ArrayList;
-import java.util.List;
 
 //Classe para arnazenar o estado das máquinas no sistema
 public class HOSEP_ControleEscravos extends SlaveControl {
@@ -14,47 +13,4 @@ public class HOSEP_ControleEscravos extends SlaveControl {
         super(Ident, ind, F, P);
     }
 
-    public String getID() {
-        return this.ID;
-    }
-
-    public int GetIndex() {
-        return this.index;
-    }
-
-    public List<Tarefa> GetFila() {
-        return this.fila;
-    }
-
-    public ArrayList<Tarefa> GetProcessador() {
-        return this.processador;
-    }
-
-    public String getStatus() {
-        return this.status;
-    }
-
-    public void setFila(final ArrayList<Tarefa> F) {
-        this.fila = F;
-    }
-
-    public void setProcessador(final ArrayList<Tarefa> P) {
-        this.processador = P;
-    }
-
-    public void setOcupado() {
-        this.status = "Ocupado";
-    }
-
-    public void setLivre() {
-        this.status = "Livre";
-    }
-
-    public void setBloqueado() {
-        this.status = "Bloqueado";
-    }
-
-    public void setIncerto() {
-        this.status = "Incerto";
-    }
 }
