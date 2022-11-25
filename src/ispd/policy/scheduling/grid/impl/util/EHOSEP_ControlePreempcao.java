707696b21e4eb78a202a00859cc2a30ae25ad0c6
Math_RPG <maugusilva.santos@gmail.com>
2022-11-25 18:10:39 -0300

    Remove redundant methods.

--- src/ispd/policy/scheduling/grid/impl/util/EHOSEP_ControlePreempcao.java
+++ src/ispd/policy/scheduling/grid/impl/util/EHOSEP_ControlePreempcao.java
@@ -2,25 +2,8 @@ package ispd.policy.scheduling.grid.impl.util;
 
 //Classe para armazenar dados sobre as preempções que ainda não terminaram
 public class EHOSEP_ControlePreempcao extends PreemptionControl {
-
     public EHOSEP_ControlePreempcao(final String userP, final int pID,
                                     final String userA, final int aID) {
         super(userP, pID, userA, aID);
-    }
-
-    public String getUsuarioPreemp() {
-        return this.usuarioPreemp;
-    }
-
-    public int getPreempID() {
-        return this.preempID;
-    }
-
-    public String getUsuarioAlloc() {
-        return this.usuarioAlloc;
-    }
-
-    public int getAllocID() {
-        return this.allocID;
     }
 }
