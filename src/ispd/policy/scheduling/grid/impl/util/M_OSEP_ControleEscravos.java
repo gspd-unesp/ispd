7011c58e140f299943e83db81e97cc608235f602
Math_RPG <maugusilva.santos@gmail.com>
2022-11-26 09:00:20 -0300

    Extract constants as variables.

--- src/ispd/policy/scheduling/grid/impl/util/M_OSEP_ControleEscravos.java
+++ src/ispd/policy/scheduling/grid/impl/util/M_OSEP_ControleEscravos.java
@@ -2,41 +2,45 @@ package ispd.policy.scheduling.grid.impl.util;
 
 public class M_OSEP_ControleEscravos {
 
+    private static final int OCCUPIED = 1;
+    private static final int FREE = 0;
+    private static final int BLOCKED = 2;
+    private static final int PREEMPTIVE = 3;
     private int contador;
 
     public M_OSEP_ControleEscravos() {
-        this.contador = 0;
+        this.contador = FREE;
     }
 
     public boolean Ocupado() {
-        return this.contador == 1;
+        return this.contador == OCCUPIED;
     }
 
     public boolean Livre() {
-        return this.contador == 0;
+        return this.contador == FREE;
     }
 
     public boolean Bloqueado() {
-        return this.contador == 2;
+        return this.contador == BLOCKED;
     }
 
     public boolean Preemp() {
-        return this.contador == 3;
+        return this.contador == PREEMPTIVE;
     }
 
     public void SetOcupado() {
-        this.contador = 1;
+        this.contador = OCCUPIED;
     }
 
     public void SetLivre() {
-        this.contador = 0;
+        this.contador = FREE;
     }
 
     public void SetBloqueado() {
-        this.contador = 2;
+        this.contador = BLOCKED;
     }
 
     public void setPreemp() {
-        this.contador = 3;
+        this.contador = PREEMPTIVE;
     }
 }
