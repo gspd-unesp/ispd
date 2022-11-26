bfbb08e6aae14627101ce00017d52a25282dc95c
Math_RPG <maugusilva.santos@gmail.com>
2022-11-26 09:45:20 -0300

    Remove redundancy.

--- src/ispd/policy/scheduling/grid/impl/util/M_OSEP_ControleEscravos.java
+++ src/ispd/policy/scheduling/grid/impl/util/M_OSEP_ControleEscravos.java
@@ -1,14 +1,4 @@
 package ispd.policy.scheduling.grid.impl.util;
 
 public class M_OSEP_ControleEscravos extends SlaveStatusControl {
-
-    private static final int OCCUPIED = 1;
-    private static final int FREE = 0;
-    private static final int BLOCKED = 2;
-    private static final int PREEMPTIVE = 3;
-    private int contador;
-
-    public M_OSEP_ControleEscravos() {
-        this.contador = FREE;
-    }
 }
