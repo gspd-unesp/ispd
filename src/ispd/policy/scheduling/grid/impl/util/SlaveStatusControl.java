1048dfd7f78c2a983ae712f433d1eb228f4af2fe
Math_RPG <maugusilva.santos@gmail.com>
2022-11-26 09:06:15 -0300

    Introduce methods for future extraction.

--- src/ispd/policy/scheduling/grid/impl/util/SlaveStatusControl.java
+++ src/ispd/policy/scheduling/grid/impl/util/SlaveStatusControl.java
@@ -1,12 +1,16 @@
 package ispd.policy.scheduling.grid.impl.util;
 
 public class SlaveStatusControl {
+    private static final String OCCUPIED = "Ocupado";
+    private static final String FREE = "Livre";
+    private static final String BLOCKED = "Bloqueado";
+    private static final String UNCERTAIN = "Incerto";
     protected final String ID;//Id da máquina escravo
     protected String status;//Estado da máquina
 
     public SlaveStatusControl(final String Ident) {
         this.ID = Ident;
-        this.status = "Livre";
+        this.status = FREE;
     }
 
     public String getID() {
@@ -17,19 +21,35 @@ public class SlaveStatusControl {
         return this.status;
     }
 
+    public boolean Ocupado() {
+        return OCCUPIED.equals(this.status);
+    }
+
+    public boolean Livre() {
+        return FREE.equals(this.status);
+    }
+
+    public boolean Bloqueado() {
+        return BLOCKED.equals(this.status);
+    }
+
+    public boolean Incerto() {
+        return UNCERTAIN.equals(this.status);
+    }
+
     public void setOcupado() {
-        this.status = "Ocupado";
+        this.status = OCCUPIED;
     }
 
     public void setLivre() {
-        this.status = "Livre";
+        this.status = FREE;
     }
 
     public void setBloqueado() {
-        this.status = "Bloqueado";
+        this.status = BLOCKED;
     }
 
     public void setIncerto() {
-        this.status = "Incerto";
+        this.status = UNCERTAIN;
     }
 }
