e8cc12f4765ed82393c8dc87b726c118bbd86499
Math_RPG <maugusilva.santos@gmail.com>
2022-11-27 09:32:52 -0300

    Remove redundant methods; make use of one to simplify.

--- src/ispd/policy/scheduling/grid/impl/util/M_OSEP_StatusUser.java
+++ src/ispd/policy/scheduling/grid/impl/util/M_OSEP_StatusUser.java
@@ -3,22 +3,13 @@ package ispd.policy.scheduling.grid.impl.util;
 public class M_OSEP_StatusUser extends EHOSEP_StatusUser {
     public M_OSEP_StatusUser(final String user, final Double perfShare) {
         super(user, perfShare);
-        this.servedPerf = 0.0;
     }
 
     public void AtualizaUso(final Double poder, final int opc) {
         if (opc == 1) {
-            this.servedPerf = this.servedPerf + poder;
+            this.addServedPerf(poder);
         } else {
-            this.servedPerf = this.servedPerf - poder;
+            this.rmServedPerf(poder);
         }
-    }
-
-    public double getPerfShare() {
-        return this.perfShare;
-    }
-
-    public double getServedPerf() {
-        return this.servedPerf;
     }
 }
