9701a1f5a9c6e285652acbf8b0150468fb35b19a
Math_RPG <maugusilva.santos@gmail.com>
2022-11-27 09:06:02 -0300

    Expand field access.

--- src/ispd/policy/scheduling/grid/impl/util/EHOSEP_StatusUser.java
+++ src/ispd/policy/scheduling/grid/impl/util/EHOSEP_StatusUser.java
@@ -3,18 +3,18 @@ package ispd.policy.scheduling.grid.impl.util;
 //Classe para dados de estado dos usuários
 public class EHOSEP_StatusUser implements Comparable<EHOSEP_StatusUser> {
 
-    private final String user;//Nome do usuario;
-    private final double perfShare;//Desempenho total das máquinas do
-    private int demanda;//Número de tarefas na fila
+    protected final String user;//Nome do usuario;
+    protected final double perfShare;//Desempenho total das máquinas do
+    protected int demanda;//Número de tarefas na fila
     // usuário
-    private double powerShare;//Consumo de energia total das máquinas do
+    protected double powerShare;//Consumo de energia total das máquinas do
     // usuário
-    private int servedNum;//Número de máquinas que atendem ao usuário
-    private double servedPerf;//Desempenho total que atende ao usuário
-    private double servedPower;//Consumo de energia total que atende ao
+    protected int servedNum;//Número de máquinas que atendem ao usuário
+    protected double servedPerf;//Desempenho total que atende ao usuário
+    protected double servedPower;//Consumo de energia total que atende ao
     // usuario
-    private double limiteConsumo;//Limite de consumo definido pelo usuario;
-    private double relacaoEficienciaSistemaPorcao;//Nova métrica para
+    protected double limiteConsumo;//Limite de consumo definido pelo usuario;
+    protected double relacaoEficienciaSistemaPorcao;//Nova métrica para
     // decisão de preempção
 
     public EHOSEP_StatusUser(final String user,
