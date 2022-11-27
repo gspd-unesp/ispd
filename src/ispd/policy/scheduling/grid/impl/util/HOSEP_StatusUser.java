899a6340c1e8a166e97ff1c07f7e33920ad61fdf
Math_RPG <maugusilva.santos@gmail.com>
2022-11-27 09:08:57 -0300

    Inline class.

--- src/ispd/policy/scheduling/grid/impl/util/HOSEP_StatusUser.java
+++
@@ -1,9 +0,0 @@
-package ispd.policy.scheduling.grid.impl.util;
-
-//Classe para dados de estado dos usuários
-public class HOSEP_StatusUser extends EHOSEP_StatusUser {
-    public HOSEP_StatusUser(final String user,
-                            final double perfShare) {
-        super(user, perfShare);
-    }
-}
