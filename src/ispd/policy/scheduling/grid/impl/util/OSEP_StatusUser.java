d0718cd82e8f140baaec65167264ac80568d37c6
Math_RPG <maugusilva.santos@gmail.com>
2022-11-27 09:51:47 -0300

    Cleanup.

--- src/ispd/policy/scheduling/grid/impl/util/OSEP_StatusUser.java
+++ src/ispd/policy/scheduling/grid/impl/util/OSEP_StatusUser.java
@@ -2,22 +2,22 @@ package ispd.policy.scheduling.grid.impl.util;
 
 import ispd.motor.filas.servidores.CS_Processamento;
 
-import java.util.List;
+import java.util.Collection;
 
 public class OSEP_StatusUser extends UserStatus {
-    private long ownerShare;//Número de máquinas do usuario
+    private final long userMachineCount;
 
-    public OSEP_StatusUser(final String user,
-                           final double perfShare,
-                           final List<CS_Processamento> slaves) {
+    public OSEP_StatusUser(
+            final String user, final double perfShare,
+            final Collection<? extends CS_Processamento> slaves) {
         super(user, perfShare);
 
-        this.ownerShare = slaves.stream()
+        this.userMachineCount = slaves.stream()
                 .filter(s -> s.getProprietario().equals(user))
                 .count();
     }
 
     public long getOwnerShare() {
-        return this.ownerShare;
+        return this.userMachineCount;
     }
 }
